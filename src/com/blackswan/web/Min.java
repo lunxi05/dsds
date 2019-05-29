ccad9748e1609e7195009b670b7c0340fc147979
chominseo <35249488+chominseo@users.noreply.github.com>
2019-05-29 15:27:45 +0900

    min

---
+++ src/com/blackswan/web/Min.java
@@ -0,0 +1,8 @@
+package com.blackswan.web;
+
+public class Min {
+private void sysout() {
+	// TODO Auto-generated method stub
+
+}
+}
---
+++ src/com/blackswan/web/dao/oracle/OracleMemberDao.java
@@ -0,0 +1,48 @@
+package com.blackswan.web.dao.oracle;
+
+import java.util.List;
+
+import com.blackswan.web.dao.MemberDao;
+import com.blackswan.web.entity.Member;
+
+public class OracleMemberDao implements MemberDao {
+
+	@Override
+	public List<Member> getList() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Member> getList(int page) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Member> getList(int page, String field, String query) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Member get(String mail) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insert(Member member) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Member member) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	
+}
