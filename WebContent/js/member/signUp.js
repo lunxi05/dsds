1634988f4cba7e90d6e9e91a70a0f7087b6091f8
chominseo <35249488+chominseo@users.noreply.github.com>
2019-06-25 13:34:37 +0900

    mins 0625

--- WebContent/js/member/signUp.js
+++ WebContent/js/member/signUp.js
@@ -1,12 +1,34 @@
-var signUp = document.getElementById('signUp');
-var func = function(){
+window.addEventListener("load", function(){
     
-    var email = document.querySelector('#email').value;
-    var name = document.querySelector('#name').value;
-    var pw = document.querySelector('#pw').value;
-    var phone = document.querySelector('#phone').value;
-
-    console.log
-
+    var signUp = document.querySelector(".signUp");
     
-};
+    signUp.onclick = function(){
+    
+    var email = document.getElementsByName("email").value;
+    var name = document.getElementsByName("name").value;
+    var pw = document.getElementsByName("pw").value;
+    var phone = document.getElementsByName("phone").value;
+     
+    if (!email){
+        alert("이메일를 입력하세요.")
+        return;
+    }
+    if (!name){
+        alert("이름를 입력하세요.")
+        return;
+    }
+    if (!pw){
+        alert("비밀번호를 입력하세요.")
+        return;
+    }
+    if (!pwc){
+        alert("비밀번호를 입력하세요.")
+        return;
+    }
+    if (!phone){
+        alert("전화번호를 입력하세요.")
+        return;
+    }
+    
+    };
+});
